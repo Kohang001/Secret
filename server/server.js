1aec507c5ee292ad86f835bb6341b085fe467ab8
Kohang001 <nathwrrthsumphlxy@gmail.com>
2026-08-01 22:34:30 +0700

    fix server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,20 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin || '*'
+  res.setHeader('Access-Control-Allow-Origin', origin)
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+
+  next()
+})
+
 app.use(express.json())
 
 const EXPECTED = {
